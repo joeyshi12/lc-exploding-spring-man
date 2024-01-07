d6cf9d38945ea96295e783889334af84ca8c0225
Joey Shi <joeyshi12@gmail.com>
2024-01-07 02:47:45 -0800

    Readded files

---
+++ SpringManKamekaze/SpringManKamekaze.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using SpringManKamekaze.Patches;
+using HarmonyLib;
+
+namespace SpringManKamekaze
+{
+    [BepInPlugin(modGUID, modName, modVersion)]
+    public class SpringManKamekazeBase : BaseUnityPlugin
+    {
+        private const string modGUID = "coolcat0.LC_SpringManKamekaze";
+        private const string modName = "LC_SpringManKamekaze";
+        private const string modVersion = "0.1.0";
+
+        private readonly Harmony harmony = new Harmony(modGUID);
+
+        private static SpringManKamekazeBase Instance;
+
+        public static ManualLogSource mls;
+
+        void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            mls = BepInEx.Logging.Logger.CreateLogSource(modGUID);
+            mls.LogInfo($"{modName} version {modVersion} has been loaded");
+
+            harmony.PatchAll(typeof(SpringManKamekazeBase));
+            harmony.PatchAll(typeof(SpringManKamekazePatch));
+        }
+    }
+}
