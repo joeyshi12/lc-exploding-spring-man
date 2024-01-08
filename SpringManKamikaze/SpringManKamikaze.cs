a1e672b6fe2cf4732cdd4a16f2e5111663f23d92
Joey Shi <joeyshi12@hotmail.com>
2024-01-07 21:25:45 -0800

    update

--- SpringManKamikaze/SpringManKamikaze.cs
+++ SpringManKamikaze/SpringManKamikaze.cs
@@ -1,6 +1,8 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using GameNetcodeStuff;
 using HarmonyLib;
+using System.Collections;
 using UnityEngine;
 
 namespace SpringManKamikaze
@@ -10,7 +12,7 @@ namespace SpringManKamikaze
     {
         private const string modGUID = "coolcat0.LC_SpringManKamikaze";
         private const string modName = "LC_SpringManKamikaze";
-        private const string modVersion = "1.0.1";
+        private const string modVersion = "1.0.2";
 
         private readonly Harmony harmony = new Harmony(modGUID);
         private static SpringManKamikazeBase Instance;
@@ -37,10 +39,102 @@ namespace SpringManKamikaze
         static bool OnCollideWithPlayerPrefix(SpringManAI __instance)
         {
             Vector3 explosionPosition = __instance.transform.position + Vector3.up;
-            Landmine.SpawnExplosion(explosionPosition, true);
-            Landmine.SpawnExplosion(explosionPosition, true);
+            CreateExplosion(explosionPosition, true, 50, 0f, 10f);
             __instance.KillEnemy(true);
             return false;
+        }
+
+        // TODO: clean up
+        public static void CreateExplosion(Vector3 explosionPosition, bool spawnExplosionEffect = false, int damage = 20, float minDamageRange = 0f, float maxDamageRange = 1f, int enemyHitForce = 6, CauseOfDeath causeOfDeath = CauseOfDeath.Blast, PlayerControllerB attacker = null)
+        {
+            Debug.Log("Spawning explosion at pos: {explosionPosition}");
+
+            Transform holder = null;
+
+            if (RoundManager.Instance != null && RoundManager.Instance.mapPropsContainer != null && RoundManager.Instance.mapPropsContainer.transform != null)
+            {
+                holder = RoundManager.Instance.mapPropsContainer.transform;
+            }
+
+            if (spawnExplosionEffect)
+            {
+                UnityEngine.Object.Instantiate(StartOfRound.Instance.explosionPrefab, explosionPosition, Quaternion.Euler(-90f, 0f, 0f), holder).SetActive(value: true);
+            }
+
+            float num = Vector3.Distance(GameNetworkManager.Instance.localPlayerController.transform.position, explosionPosition);
+            if (num < 14f)
+            {
+                HUDManager.Instance.ShakeCamera(ScreenShakeType.Big);
+            }
+            else if (num < 25f)
+            {
+                HUDManager.Instance.ShakeCamera(ScreenShakeType.Small);
+            }
+
+            Collider[] array = Physics.OverlapSphere(explosionPosition, maxDamageRange, 2621448, QueryTriggerInteraction.Collide);
+            PlayerControllerB playerControllerB = null;
+            for (int i = 0; i < array.Length; i++)
+            {
+                float num2 = Vector3.Distance(explosionPosition, array[i].transform.position);
+                if (num2 > 4f && Physics.Linecast(explosionPosition, array[i].transform.position + Vector3.up * 0.3f, 256, QueryTriggerInteraction.Ignore))
+                {
+                    continue;
+                }
+
+                if (array[i].gameObject.layer == 3)
+                {
+                    playerControllerB = array[i].gameObject.GetComponent<PlayerControllerB>();
+                    if (playerControllerB != null && playerControllerB.IsOwner)
+                    {
+                        // calculate damage based on distance, so if player is minDamageRange or closer, they take full damage
+                        // if player is maxDamageRange or further, they take no damage
+                        // distance is num2
+                        float damageMultiplier = 1f - Mathf.Clamp01((num2 - minDamageRange) / (maxDamageRange - minDamageRange));
+
+                        playerControllerB.DamagePlayer((int)(damage * damageMultiplier), causeOfDeath: causeOfDeath);
+                    }
+                }
+                else if (array[i].gameObject.layer == 21)
+                {
+                    Landmine componentInChildren = array[i].gameObject.GetComponentInChildren<Landmine>();
+                    if (componentInChildren != null && !componentInChildren.hasExploded && num2 < 6f)
+                    {
+                        Debug.Log("Setting off other mine");
+                        componentInChildren.StartCoroutine(TriggerOtherMineDelayed(componentInChildren));
+                    }
+                }
+                else if (array[i].gameObject.layer == 19)
+                {
+                    EnemyAICollisionDetect componentInChildren2 = array[i].gameObject.GetComponentInChildren<EnemyAICollisionDetect>();
+                    if (componentInChildren2 != null && componentInChildren2.mainScript.IsOwner && num2 < 4.5f)
+                    {
+                        componentInChildren2.mainScript.HitEnemyOnLocalClient(enemyHitForce, playerWhoHit: attacker);
+                    }
+                }
+            }
+
+            int num3 = ~LayerMask.GetMask("Room");
+            num3 = ~LayerMask.GetMask("Colliders");
+            array = Physics.OverlapSphere(explosionPosition, 10f, num3);
+            for (int j = 0; j < array.Length; j++)
+            {
+                Rigidbody component = array[j].GetComponent<Rigidbody>();
+                if (component != null)
+                {
+                    component.AddExplosionForce(70f, explosionPosition, 10f);
+                }
+            }
+        }
+
+        public static IEnumerator TriggerOtherMineDelayed(Landmine mine)
+        {
+            if (!mine.hasExploded)
+            {
+                mine.mineAudio.pitch = UnityEngine.Random.Range(0.75f, 1.07f);
+                mine.hasExploded = true;
+                yield return new WaitForSeconds(0.2f);
+                mine.SetOffMineAnimation();
+            }
         }
     }
 }
