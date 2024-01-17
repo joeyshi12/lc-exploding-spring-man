8f45f4bdb4702011bc6831a9033b5c9cb61c4a85
Joey Shi <joeyshi12@hotmail.com>
2024-01-16 21:32:22 -0800

    Update scripts

---
+++ Build-Game.ps1
@@ -0,0 +1,13 @@
+# r2modman profile to test with
+$Profile = "Dev"
+$ProfilePath = "$env:APPDATA\r2modmanPlus-local\LethalCompany\profiles\$Profile"
+$DoorstopTarget = "$ProfilePath\BepInEx\core\BepInEx.Preloader.dll"
+
+# Build and copy dll to profile plugins folder
+dotnet build
+$OutputPath = ".\SpringManKamikaze\bin\Debug\net7.0\SpringManKamiKaze.dll"
+$PluginPath = "$ProfilePath\BepInEx\plugins"
+
+Copy-Item "$PluginPath" ".\Thunderstore" -Force
+Copy-Item "$OutputPath" "$PluginPath" -Force
+Write-Host "Copied plugin to $PluginPath"
--- Run-Game.ps1
+++ Run-Game.ps1
@@ -1,11 +1,7 @@
 # r2modman profile to test with
 $Profile = "Dev"
-$ProfilePath = "C:\Users\Joey Shi\AppData\Roaming\r2modmanPlus-local\LethalCompany\profiles\$Profile"
+$ProfilePath = "$env:APPDATA\r2modmanPlus-local\LethalCompany\profiles\$Profile"
 $DoorstopTarget = "$ProfilePath\BepInEx\core\BepInEx.Preloader.dll"
-
-# Build and copy dll to profile plugins folder
-dotnet build
-Copy-Item ".\SpringManKamikaze\bin\Debug\net7.0\SpringManKamiKaze.dll" "$ProfilePath\BepInEx\plugins"
 
 # Start game
 & 'C:\Program Files (x86)\Steam\steamapps\common\Lethal Company\Lethal Company.exe' `
