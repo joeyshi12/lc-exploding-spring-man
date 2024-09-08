c71444ccda71d06b59b074f015a191f9be193eb3
Joey Shi <joeyshi12@hotmail.com>
2024-09-07 20:35:34 -0700

    update output path

--- Build-Game.ps1
+++ Build-Game.ps1
@@ -4,7 +4,7 @@ $ProfilePath = "$env:APPDATA\r2modmanPlus-local\LethalCompany\profiles\$Profile"
 
 # Build and copy dll to profile plugins folder
 dotnet build
-$OutputPath = ".\SpringManKamikaze\bin\Debug\net7.0\SpringManKamiKaze.dll"
+$OutputPath = ".\SpringManKamikaze\bin\Debug\net7.0\SpringManKamikaze.dll"
 $PluginPath = "$ProfilePath\BepInEx\plugins"
 
 Copy-Item "$OutputPath" ".\Thunderstore" -Force
